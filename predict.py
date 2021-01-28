7685f42f7151aa1a3d742fd160656125317ea94e
Andoni Beristain Iraola <aberistain@vicomtech.org>
2021-01-28 16:56:33 +0100

    [STY] pep8 [ENH] Improved path management, and adapted to ISIC2018 local

--- predict.py
+++ predict.py
@@ -1,30 +1,35 @@
-
 import os
-import numpy as np
+from pathlib import Path
+from glob import glob
+
 import cv2
+import numpy as np
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import CustomObjectScope
-from glob import glob
 from tqdm import tqdm
-from sklearn.model_selection import train_test_split
-from utils import *
+
 from train import tf_dataset
+from utils import *
+
 
 def read_image(x):
     image = cv2.imread(x, cv2.IMREAD_COLOR)
-    image = np.clip(image - np.median(image)+127, 0, 255)
-    image = image/255.0
+    image = np.clip(image - np.median(image) + 127, 0, 255)
+    image = image / 255.0
     image = image.astype(np.float32)
     image = np.expand_dims(image, axis=0)
     return image
 
+
 def read_mask(y):
     mask = cv2.imread(y, cv2.IMREAD_GRAYSCALE)
     mask = mask.astype(np.float32)
-    mask = mask/255.0
+    mask = mask / 255.0
     mask = np.expand_dims(mask, axis=-1)
     return mask
+
 
 def mask_to_3d(mask):
     mask = np.squeeze(mask)
@@ -32,12 +37,14 @@ def mask_to_3d(mask):
     mask = np.transpose(mask, (1, 2, 0))
     return mask
 
+
 def parse(y_pred):
     y_pred = np.expand_dims(y_pred, axis=-1)
     y_pred = y_pred[..., -1]
     y_pred = y_pred.astype(np.float32)
     y_pred = np.expand_dims(y_pred, axis=-1)
     return y_pred
+
 
 def evaluate_normal(model, x_data, y_data):
     THRESHOLD = 0.5
@@ -49,9 +56,9 @@ def evaluate_normal(model, x_data, y_data):
 
         y_pred1 = parse(model.predict(x)[0][..., -2])
         y_pred2 = parse(model.predict(x)[0][..., -1])
-        
+
         line = np.ones((h, 10, 3)) * 255.0
-        
+
         all_images = [
             x[0] * 255.0, line,
             mask_to_3d(y) * 255.0, line,
@@ -62,32 +69,39 @@ def evaluate_normal(model, x_data, y_data):
 
         cv2.imwrite(f"results/{i}.png", mask)
 
+
 smooth = 1.
+
+
 def dice_coef(y_true, y_pred):
     y_true_f = tf.keras.layers.Flatten()(y_true)
     y_pred_f = tf.keras.layers.Flatten()(y_pred)
     intersection = tf.reduce_sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
 
+
 def dice_loss(y_true, y_pred):
     return 1.0 - dice_coef(y_true, y_pred)
+
 
 if __name__ == "__main__":
     np.random.seed(42)
     tf.random.set_seed(42)
     create_dir("results/")
 
-    batch_size = 8
+    batch_size = 2
 
-    test_path = "../1/new_data/test/"
+    test_path = str(Path(__file__).parent.joinpath('dataset', 'ISIC_2018'))
+    model_path = str(Path(__file__).parent.joinpath('files', 'model.h5'))
+
     test_x = sorted(glob(os.path.join(test_path, "image", "*.jpg")))
-    test_y = sorted(glob(os.path.join(test_path, "mask", "*.jpg")))
+    test_y = sorted(glob(os.path.join(test_path, "mask", "*.png")))
     test_dataset = tf_dataset(test_x, test_y, batch=batch_size)
 
-    test_steps = (len(test_x)//batch_size)
+    test_steps = (len(test_x) // batch_size)
     if len(test_x) % batch_size != 0:
         test_steps += 1
 
-    model = load_model_weight("files/model.h5")
+    model = load_model_weight(model_path)
     model.evaluate(test_dataset, steps=test_steps)
     evaluate_normal(model, test_x, test_y)
